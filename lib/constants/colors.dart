71cb57c9bb40205525a8d943c63404754a44c327
adrianuspandu <pwicaks@gmail.com>
2025-01-05 20:50:22 +0100

    add app color constants and assets, update home screen layout

---
+++ lib/constants/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color habitBG = Color(0xFFFFF4C0);
+const Color habitPrimary = Color(0xFFFF9D00);
+const Color habitSecondary = Color(0xFFFFEEA3);
+const Color habitAccent = Color(0xFFE7CA8F);
+const Color habitAccent2 = Color(0xFFEB5600);
+const Color habitText = Color(0xFF181611);
--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,6 +1,7 @@
 // lib/screens/main_screen.dart
 
 import 'package:flutter/material.dart';
+import 'package:habit_tracker_app/constants/colors.dart';
 
 import 'home_screen.dart';
 import 'progress_screen.dart';
@@ -35,42 +36,109 @@ class _MainScreenState extends State<MainScreen> {
           // Переходим на экран добавления привычки
           Navigator.pushNamed(context, '/addHabit');
         },
-        backgroundColor: Theme.of(context).primaryColor,
-        child: const Icon(Icons.add),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        child: Image.asset('lib/assets/icons/add_habit_button.png'),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
-
-      bottomNavigationBar: BottomAppBar(
-        shape: const CircularNotchedRectangle(),
-        notchMargin: 8.0,
-        color: Colors.white,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            IconButton(
-              icon: const Icon(Icons.home),
-              color: _currentIndex == 0 ? Theme.of(context).primaryColor : Colors.grey,
-              onPressed: () => setState(() => _currentIndex = 0),
+      bottomNavigationBar: NavigationBar(
+        labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
+        onDestinationSelected: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        backgroundColor: habitSecondary,
+        indicatorColor: Colors.transparent,
+        selectedIndex: _currentIndex,
+        destinations: [
+          NavigationDestination(
+            selectedIcon: Image.asset(
+              'lib/assets/icons/home_on.png',
+              width: 47,
+              height: 47,
             ),
-            IconButton(
-              icon: const Icon(Icons.bar_chart),
-              color: _currentIndex == 1 ? Theme.of(context).primaryColor : Colors.grey,
-              onPressed: () => setState(() => _currentIndex = 1),
+            icon: Image.asset(
+              'lib/assets/icons/home.png',
+              width: 47,
+              height: 47,
             ),
-            const SizedBox(width: 48), // пространство для FAB
-            IconButton(
-              icon: const Icon(Icons.explore),
-              color: _currentIndex == 2 ? Theme.of(context).primaryColor : Colors.grey,
-              onPressed: () => setState(() => _currentIndex = 2),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            selectedIcon: Image.asset(
+              'lib/assets/icons/progress_on.png',
+              width: 47,
+              height: 47,
             ),
-            IconButton(
-              icon: const Icon(Icons.emoji_events), // Achievements
-              color: _currentIndex == 3 ? Theme.of(context).primaryColor : Colors.grey,
-              onPressed: () => setState(() => _currentIndex = 3),
+            icon: Image.asset(
+              'lib/assets/icons/progress.png',
+              width: 47,
+              height: 47,
             ),
-          ],
-        ),
+            label: 'Notifications',
+          ),
+          NavigationDestination(
+            selectedIcon: Image.asset(
+              'lib/assets/icons/explore_on.png',
+              width: 47,
+              height: 47,
+            ),
+            icon: Image.asset(
+              'lib/assets/icons/explore.png',
+              width: 47,
+              height: 47,
+            ),
+            label: 'Notifications',
+          ),
+          NavigationDestination(
+            selectedIcon: Image.asset(
+              'lib/assets/icons/settings_on.png',
+              width: 47,
+              height: 47,
+            ),
+            icon: Image.asset(
+              'lib/assets/icons/settings.png',
+              width: 47,
+              height: 47,
+            ),
+            label: 'Messages',
+          ),
+        ],
       ),
+
+      // bottomNavigationBar:
+      // BottomAppBar(
+      //   shape: const CircularNotchedRectangle(),
+      //   notchMargin: 8.0,
+      //   color: Colors.white,
+      //   child: Row(
+      //     mainAxisAlignment: MainAxisAlignment.spaceAround,
+      //     children: [
+      //       IconButton(
+      //         icon: const Icon(Icons.home),
+      //         color: _currentIndex == 0 ? Theme.of(context).primaryColor : Colors.grey,
+      //         onPressed: () => setState(() => _currentIndex = 0),
+      //       ),
+      //       IconButton(
+      //         icon: const Icon(Icons.bar_chart),
+      //         color: _currentIndex == 1 ? Theme.of(context).primaryColor : Colors.grey,
+      //         onPressed: () => setState(() => _currentIndex = 1),
+      //       ),
+      //       const SizedBox(width: 48), // пространство для FAB
+      //       IconButton(
+      //         icon: const Icon(Icons.explore),
+      //         color: _currentIndex == 2 ? Theme.of(context).primaryColor : Colors.grey,
+      //         onPressed: () => setState(() => _currentIndex = 2),
+      //       ),
+      //       IconButton(
+      //         icon: const Icon(Icons.emoji_events), // Achievements
+      //         color: _currentIndex == 3 ? Theme.of(context).primaryColor : Colors.grey,
+      //         onPressed: () => setState(() => _currentIndex = 3),
+      //       ),
+      //     ],
+      //   ),
+      // ),
     );
   }
 }
